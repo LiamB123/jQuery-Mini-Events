1f86a8ac19d9979f2e089162686e55658d2c353e
Ubuntu <ubuntu@ip-172-31-95-246.ec2.internal>
2019-08-18 18:32:11 +0000

    jQuery

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+ //
+ // add your jQuery code here
+ @(document).mouseenter(function(){
+  $("button").mouseenter(removeClass('makeRed').addClass('makeBorder'));
+   $("button").mouseleave(removeClass('makeBorder').addClass('makeRed'));
+  
+ 
+
+
+}); 
